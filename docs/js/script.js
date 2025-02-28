9b448dfa58c1f2d93d4d667b26aa1684d1bae0e9
Harsh Prajapati <harshprajapaticoding@gmail.com>
2025-02-28 22:06:33 +0530

    added animated menu in all the pages

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,8 @@
 
 // common animations 
 const btns = document.querySelectorAll('.btn');
+const menuLinks = document.querySelectorAll('.menu-links'); // for menu animation 
+
 
 // button animation 
 btns.forEach((btn) => {
@@ -40,5 +42,79 @@ document.addEventListener('DOMContentLoaded', () => {
         opacity: 0,
         duration: 1,
         x: "100%"
+    })
+})
+
+// Showing menu --------------------------------------------------------------------------------------------------------
+const menuIcon = document.querySelector('.menu-icon');
+const crossIcon = document.querySelector('.cross-icon');
+const menu = document.querySelector('#menu');
+let isShowingmenu = true;
+const showingmenu = () => {
+    const menuImage = document.querySelector('.menu-left img');
+    if (isShowingmenu) {
+        isShowingmenu = false;
+
+        let tl = gsap.timeline();
+
+        tl
+            .to(menuIcon, {
+                scale: 2,
+                opacity: 0,
+                duration: 0.5
+            })
+            .to(menu, {
+                y: "0%",
+                duration: 1
+            })
+            .from(crossIcon, {
+                opacity: 0,
+                rotate: 360,
+                duration: 0.5
+            })
+            .from(menuImage, {
+                opacity: 0,
+                x: "-100%",
+                duration: 0.5
+            })
+            .from(".menu-links a", {
+                y: "100%",
+                duration: 1
+            })
+
+    } else {
+        isShowingmenu = true;
+
+        let tl = gsap.timeline();
+
+        tl
+            .to(menu, {
+                y: "-100%",
+                duration: 1
+            })
+            .to(menuIcon, {
+                scale: 1,
+                opacity: 1,
+                duration: 0.5
+            })
+
+    }
+}
+menuIcon.addEventListener('click', showingmenu);
+crossIcon.addEventListener('click', showingmenu);
+
+// menulink animation --------------------------------------------------------------------------------------------------------
+menuLinks.forEach((link) => {
+    link.addEventListener("mouseenter", () => {
+        cursor.classList.add("opacity-50");
+        cursor.textContent = "menu"
+        let element = link.querySelector('a');
+        gsap.timeline()
+            .to(element, { y: 20, opacity: 0, duration: 0.2 })
+            .to(element, { y: 0, opacity: 1, duration: 0.2 })
+    })
+    link.addEventListener("mouseleave", () => {
+        cursor.classList.remove("opacity-50");
+        cursor.textContent = "dotHP"
     })
 })
